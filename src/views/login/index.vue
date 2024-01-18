e64b807857b4c721264d65d3efb7fe4da5d89ed2
sxy <3353673967@qq.com>
2024-01-18 20:32:36 +0800

    aaa

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,158 +1,96 @@
 <template>
   <div class="login-container">
-    <div class="logo" />
-    <div class="form">
-      <h1>手机号登录</h1>
-      <el-card shadow="never" class="login-card">
-        <!--登录表单-->
-        <el-form ref="form" :model="form" :rules="rules">
-          <el-form-item prop="mobile">
-            <el-input
-              v-model.trim="form.mobile"
-              placeholder="请输入手机号"
-            />
+<div class="left">
+
+</div>
+<div class="right">
+  <h1>手机号登录</h1>
+  <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+  <el-form-item prop="username">
+    <el-input v-model="ruleForm.username" placeholder="请输入用户名"></el-input>
+  </el-form-item>
+  <el-form-item prop="password">
+    <el-input v-model="ruleForm.password"  placeholder="请输入密码"></el-input>
+  </el-form-item>
+  <el-form-item>
+            <el-checkbox v-model="ruleForm.isAgree">用户平台使用协议</el-checkbox>
           </el-form-item>
-          <el-form-item prop="password">
-            <el-input
-              v-model.trim="form.password"
-              show-password
-              placeholder="请输入密码"
-            />
-          </el-form-item>
-          <el-form-item prop="isAgree">
-            <el-checkbox v-model="form.isAgree">用户平台使用协议</el-checkbox>
-          </el-form-item>
-          <el-form-item>
-            <el-button
-              type="primary"
-              style="width: 350px"
-              @click="submit"
-            >登录</el-button>
-          </el-form-item>
-        </el-form>
-      </el-card>
-    </div>
+  <el-form-item>
+    <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+  </el-form-item>
+</el-form>
+</div>
   </div>
 </template>
 
 <script>
+import { validUsername } from '@/utils/validate'
+
 export default {
-  name: 'Login',
-  data() {
-    return {
-      // 表单绑定数据
-      form: {
-        mobile: '',
-        password: '',
-        isAgree: false
-      },
-      // 表单校验规则
-      rules: {
-        mobile: [
-          { required: true, message: '请输入手机号', trigger: 'blur' },
+    data() {
+      return {
+        ruleForm: {
+username:"",
+password:"",
+isAgree:false
+        },
+        rules: {
+          username: [
+            { required: true, message: '请输入手机号', trigger: 'blur' },
           {
-            pattern: /^1[3-9]\d{9}$/,
-            message: '手机号格式不正确',
-            trigger: 'blur'
+            pattern:/^1[3-9]\d{9}$/,
+            message: '手机号格式不正确', trigger: 'blur'
           }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-          {
-            min: 6,
-            max: 16,
-            message: '密码长度应该为6-16位之间',
-            trigger: 'blur'
-          }
-        ],
-        isAgree: [
-          {
-            validator: (rule, value, callback) => {
-              // 当前的规则
-              // value 当前v-mode所绑定的值
-              // callback之必须要执行的函数  promise resove reject
-              // callback()
-              // callback(new Error("您当前的规则"))
-              // rule规则
-              // value检查的数据 true/false
-              // callback 函数 执行这个函数
-              // 成功执行callback 失败也执行callback(错误对象 new Error(错误信息))
-              value ? callback() : callback(new Error('没有勾选用户平台协议'))
-            }
-          }
-        ]
-      }
-    }
-  },
-  methods: {
-    submit() {
-      // 校验整个表单
-      this.$refs.form.validate((valid) => {
-        // if (!valid) return
-        if (valid) {
-          // 执行登录相关的操作
+          ],
+          password:[
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 1, max: 6, message: '长度在 1 到 6 个字符', trigger: 'blur' }
+          ],
+isAgree:[
+  {validator:(rule,value,callback)=>{
+    // rule:当前规则
+    // value:当前绑定的值
+    // callback:必须执行的函数
+    value ? callback() : callback(new Error('请同意用户平台使用协议'))
+  }}
+]
         }
-      })
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
     }
   }
-}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .login-container {
-  display: flex;
-  align-items: stretch;
+  width: 100vw;
   height: 100vh;
-  .logo {
+  display: flex;
+  .left{
+    height: 100vh;
     flex: 3;
-    background: rgba(38, 72, 176) url(../../assets/common/login_back.png)
-      no-repeat center / cover;
-    border-top-right-radius: 60px;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    justify-content: center;
-    padding: 0 100px;
-    .icon {
-      background: url(../../assets/common/logo.png) no-repeat 70px center /
-        contain;
-      width: 300px;
-      height: 50px;
-      margin-bottom: 50px;
-    }
-    p {
-      color: #fff;
-      font-size: 18px;
-      margin-top: 20px;
-      width: 300px;
-      text-align: center;
-    }
+    background: url("../../assets/common/login_back.png");
+    background-size: 100%;
   }
-  .form {
-    flex: 2;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding-left: 100px;
-    .el-card {
-      border: none;
-      padding: 0;
-    }
-    h1 {
-      padding-left: 20px;
-      font-size: 24px;
-    }
-    .el-input {
-      width: 350px;
-      height: 44px;
-      .el-input__inner {
-        // background: #f4f5fb;
-        background: #fff;
-      }
-    }
-    .el-checkbox {
-      color: #606266;
-    }
+}
+.right{
+  padding: 10% 0;
+  height: 100vh;
+  flex: 2;
+  box-sizing: border-box;
+  h1{
+    margin-left: 100px;
   }
 }
 </style>
